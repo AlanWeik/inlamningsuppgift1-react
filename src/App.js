114a8a786b8fc452146a36417716fad04fd8c8ac
Alan Weik <alaan.m@hotmail.com>
2021-12-01 15:38:10 +0100

    Added API call functionality using useEffect.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Routing } from './routes/Routing';
 import { Navigation } from "./components/Navigation";
-
+import { GlobalStyles } from "./shared/styles/GlobalStyles";
 
 function App() {
   return (
     <div>
+      <GlobalStyles />
       <Routing>
         <Navigation />
       </Routing>
